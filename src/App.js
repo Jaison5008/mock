71db6c48c69f4fd3062d9b1201bb3a2a41eab00a
Jaison5008 <jaison.eee@gmail.com>
2024-03-04 18:58:21 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,93 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import React, { useState } from 'react'
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Table from 'react-bootstrap/Table';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+ 
+ 
+
+const [name,setName]=useState('');
+const [email,setEmail]=useState('');
+const [mobile,setMobile]=useState('');
+const [password,setPassword]=useState('');  
+const [items,setItem]=useState([]); 
+console.log(items)
+const submiting=(e)=>{  
+  e.preventDefault() 
+  setItem(([{name,email,mobile,password}]))
+   setName('') 
+   setEmail('')  
+   setMobile('')
+   setPassword('')
+}
+
+  return (<div className='box'>
+    <Form> 
+      <Form.Group className="mb-3" controlId="formBasicName">
+        <Form.Label>Name</Form.Label>
+        <Form.Control type="text" placeholder="Enter name" value={name} onChange={(e)=>setName(e.target.value)} />
+        </Form.Group> 
+
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="text" placeholder="Enter email" value={email} onChange={(e)=>setEmail(e.target.value)} />
+       
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="text" placeholder="Password" value={password} onChange={(e)=>setPassword(e.target.value)} />
+      </Form.Group>  
+
+      <Form.Group className="mb-3" controlId="formBasicMobile">
+        <Form.Label>Mobile</Form.Label>
+        <Form.Control type="text" placeholder="Mobile" value={mobile} onChange={(e)=>setMobile(e.target.value)} />
+      </Form.Group> 
+
+      
+      <Button variant="primary" type="submit" onClick={(e)=>submiting(e)}>
+        Submit
+      </Button> 
+
+      
+    </Form>  
+
+    <div>   
+
+      
+
+    </div> 
+
+    <Table striped bordered hover>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Email</th>
+          <th>Mobile</th>
+          <th>Password</th>
+        </tr>
+      </thead>
+      <tbody> 
+      {items.length>0?items.map((item)=>
+   
+    
+   
+        <tr>
+          <td>   {item.name}</td>
+          <td>     {item.name}</td>
+          <td>   {item.name}</td>
+          <td>    {item.name}</td>
+        </tr> ):""}
+        
+      </tbody>
+    </Table>
+    
+     </div>
   );
 }
+
+
 
 export default App;
